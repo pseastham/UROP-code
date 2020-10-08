26eac419298d13bd72f2e14b74093fe77a932bbf
mtutko <71610036+mtutko@users.noreply.github.com>
2020-10-08 12:58:22 -0400

    Update cpp_pull.cpp
    Pull request

--- first-pull-request/cpp_pull.cpp
+++ first-pull-request/cpp_pull.cpp
@@ -5,8 +5,7 @@ int main()
 {
     cout << "This is an example output" << endl;
 
-    // Place for Mason to submit pull request 
-
+    cout << "This is an example pull request." << endl; // Place for Mason to submit pull request (if he so chooses) 
     // 
 
     return 0;
