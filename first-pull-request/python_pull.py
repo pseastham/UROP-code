6318c7805563bd70cfabfd850af01ed06376563f
cjcrowder1 <71831387+cjcrowder1@users.noreply.github.com>
2020-10-13 13:23:37 -0400

    Update python_pull.py

--- first-pull-request/python_pull.py
+++ first-pull-request/python_pull.py
@@ -4,7 +4,7 @@ print("This is an example piece of code!")
 
 # Place for Caroline to submit pull request 
 
-# 
+print("This is Caroline's example piece of code!")
 
 # Place for Krystal to submit pull request 
 
